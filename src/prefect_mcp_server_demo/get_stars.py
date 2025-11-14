0cee28a69cde5d0f28a193b352a51ee4e38cc4f0
zzstoatzz <thrast36@gmail.com>
2025-11-14 11:29:58 -0600

    bugs

--- src/prefect_mcp_server_demo/get_stars.py
+++ src/prefect_mcp_server_demo/get_stars.py
@@ -5,10 +5,11 @@ import httpx
 @task
 def get_stars(repo: str):
     url = f"https://api.github.com/repos/{repo}"
-    count = httpx.get(url).json()["stargazers_count"]
+    count = httpx.get(url).json()["stargazer_count"]
     print(f"{repo} has {count} stars!")
 
 
 @flow(name="GitHub Stars", log_prints=True)
 def github_stars(repos: list[str]):
-    get_stars.map(repos).wait()
+    for repo in repos:
+        get_stars(repo)
