27719f965ed778059e50971d7459bdc8e01f12af
zzstoatzz <thrast36@gmail.com>
2025-11-14 11:10:52 -0600

    introduce bug

--- src/prefect_mcp_server_demo/get_stars.py
+++ src/prefect_mcp_server_demo/get_stars.py
@@ -5,21 +5,10 @@ import httpx
 @task
 def get_stars(repo: str):
     url = f"https://api.github.com/repos/{repo}"
-    count = httpx.get(url).json()["stargazers_count"]
+    count = httpx.get(url).json()["stargazer_count"]
     print(f"{repo} has {count} stars!")
 
 
 @flow(name="GitHub Stars", log_prints=True)
 def github_stars(repos: list[str]):
     get_stars.map(repos).wait()
-
-
-if __name__ == "__main__":
-    github_stars(
-        [
-            "PrefectHQ/prefect",
-            "PrefectHQ/prefect-mcp-server",
-            "jlowin/fastmcp",
-            "MarshalX/atproto",
-        ]
-    )
